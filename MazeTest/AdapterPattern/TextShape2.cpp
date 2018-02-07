789648d9ab2b10cf1e0c422fed2004b8310b345a
air seok <wizard_i@naver.com>
2018-02-07 09:24:42 +0900

    adapter complete

---
+++ MazeTest/AdapterPattern/TextShape2.cpp
@@ -0,0 +1,9 @@
+//
+//  TextShape2.cpp
+//  AdapterPattern
+//
+//  Created by air seok on 2018. 2. 7..
+//  Copyright © 2018년 air seok. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ MazeTest/AdapterPattern/TextShape2.h
@@ -0,0 +1,13 @@
+//
+//  TextShape2.h
+//  AdapterPattern
+//
+//  Created by air seok on 2018. 2. 7..
+//  Copyright © 2018년 air seok. All rights reserved.
+//
+
+#ifndef TextShape2_h
+#define TextShape2_h
+
+
+#endif /* TextShape2_h */
